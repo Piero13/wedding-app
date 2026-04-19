cf4c36bc2f93eac72596529ecdf941e8bc6610d9
Pierre Fasce <lemonpiero@gmail.com>
2026-04-19 10:25:36 +0200

    add auth admin

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../services/supabase/supabaseClient";
+import { AuthContext } from "./AuthContext";
+
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_, session) => {
+        setUser(session?.user || null);
+      }
+    );
+
+    return () => listener.subscription.unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
