33c4a60577f66ad482d834d80bf0bdeb2d845513
Pierre Fasce <lemonpiero@gmail.com>
2026-04-19 15:17:12 +0200

    add Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,125 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../services/supabase/supabaseClient";
+import { validateAccessCode } from "../services/auth/accessService";
+import { Form, Button, Container, Alert } from "react-bootstrap";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+
+/**
+ * Login page for admin and guests
+ */
 export default function Login() {
-    return (
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    code: ""
+  });
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleAdminLogin = async () => {
+    setError(null);
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: form.email,
+      password: form.password,
+    });
+
+    if (error) {
+        setError(error.message)
+        return
+    };
+
+    navigate("/admin")
+  };
+
+  const handleGuestLogin = async () => {
+    const data = await validateAccessCode(form.code);
+
+    if (!data) {
+      setError("Invalid access code");
+      return;
+    }
+
+    localStorage.setItem("guest_access", form.code);
+    navigate("/gallery");
+  };
+
+  return (
+    <Container className="py-5">
+      <h2 className="text-center mb-4">Connexion</h2>
+
+      <div className="text-center mb-3">
+        <Button onClick={() => setIsAdmin(false)}>Invité</Button>{" "}
+        <Button onClick={() => setIsAdmin(true)}>Admin</Button>
+      </div>
+
+      {error && <Alert variant="danger">{error}</Alert>}
+
+      {isAdmin ? (
         <>
-            <h1>Login</h1>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+
+            <Form.Control
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Mot de passe</Form.Label>
+
+            <div className="position-relative">
+                <Form.Control
+                    type={showPassword ? 'text' : 'password'}
+                    name="password"
+                    placeholder="Mot de passe"
+                    onChange={handleChange}
+                    required
+                />
+                <span
+                    onClick={() => setShowPassword(!showPassword)}
+                    style={{
+                        transform: 'translateY(-50%)',
+                        cursor: 'pointer',
+                        right: '10px',
+                        opacity: 0.7,
+                    }}
+                    onMouseEnter={(e) => e.currentTarget.style.opacity = 1}
+                    onMouseLeave={(e) => e.currentTarget.style.opacity = 0.7}
+                    className='d-flex justify-content-center align-items-center position-absolute top-50'
+                >
+                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </span>
+            </div>
+          </Form.Group>
+
+          <Button onClick={handleAdminLogin}>Connexion admin</Button>
         </>
-    )
+      ) : (
+        <>
+          <Form.Group className="mb-3">
+            <Form.Label>Code d'accès</Form.Label>
+
+            <Form.Control
+              name="code"
+              placeholder="Code d'accès"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Button onClick={handleGuestLogin}>Entrer</Button>
+        </>
+      )}
+    </Container>
+  );
 }
